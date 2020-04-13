d9cedffd8790a32022c7e418aef7a175f5063d24
Mrinal <mrinalhaque99@gmail.com>
2020-04-13 23:59:42 +0600

    js file enqueue

--- plugin.php
+++ plugin.php
@@ -2,3 +2,9 @@
 /**
  * Plugin Name: Guten block development brushup
  */
+
+ add_action( 'enqueue_block_editor_assets', 'mrinal_enqueue_block' );
+
+ function mrinal_enqueue_block() {
+     wp_enque_script( 'mrinal-block', plugins_url( 'block.js', __FILE__ ), array( 'wp-blocks', 'wp-element' ) );
+ }
