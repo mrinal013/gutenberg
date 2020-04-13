74d0aa2d419ac464c2dcaae7f0aa3f5138e52cc3
Mrinal <mrinalhaque99@gmail.com>
2020-04-14 01:49:30 +0600

    simple alert block

--- plugin.php
+++ plugin.php
@@ -3,8 +3,12 @@
  * Plugin Name: Guten block development brushup
  */
 
- add_action( 'enqueue_block_editor_assets', 'mrinal_enqueue_block' );
+add_action( 'enqueue_block_editor_assets', 'mrinal_enqueue_blocks' );
+function mrinal_enqueue_blocks() {
+    wp_enqueue_script( 'mrinal-block', plugins_url( 'block.js', __FILE__ ), [ 'wp-blocks', 'wp-element' ] );
+}
 
- function mrinal_enqueue_block() {
-     wp_enque_script( 'mrinal-block', plugins_url( 'block.js', __FILE__ ), array( 'wp-blocks', 'wp-element' ) );
- }
+add_action( 'enque_block_assets', 'mrinal_enqueue_style' );
+function mrinal_enqueue_style() {
+    wp_enqueue_style( 'mrinal-block-style', plugins_url( 'block.css', __FILE__ ), [], filemtime( plugin_dir_path( __FILE__ ) . 'block.css') );
+}
